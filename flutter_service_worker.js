b5cfe5255ad5f351d1b67f0b437f94357c3324c6
MahmoudKhalid <darkness20016@gmail.com>
2024-05-12 18:51:57 +0200

    New update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -67,8 +67,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "d5a4f1f6fbef6f202f4abcd24fae0811
 "icons/Icon-512.png": "c08305b8702a246b017ade24412346f2",
 "icons/Icon-maskable-192.png": "f472405902b99b6ef988383113650d22",
 "icons/Icon-maskable-512.png": "c08305b8702a246b017ade24412346f2",
-"index.html": "ac1cefcdf5a9f50f3d5ea5c9b4007c79",
-"/": "ac1cefcdf5a9f50f3d5ea5c9b4007c79",
+"index.html": "ced81823404d45ed2b8752467ce2ac88",
+"/": "ced81823404d45ed2b8752467ce2ac88",
 "main.dart.js": "8d34fb21397ac21b5988be069e4202d7",
 "manifest.json": "db11d8c0600a877f56a2dbea3988483c",
 "version.json": "9d624d695ab490aec928a8cc473ee509"};
